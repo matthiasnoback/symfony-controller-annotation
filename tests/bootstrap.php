f40c494f822653bcacd21585b0c4cbad584fd4f9
Matthias Noback <matthiasnoback@gmail.com>
2014-02-17 08:54:20 +0100

    Autoloader should only load src classes
    Fixes #2.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
-$autoloader = require __DIR__.'/../vendor/autoload.php';
-
 use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$autoloader = require __DIR__.'/../vendor/autoload.php';
+/* @var $autoloader Composer\Autoload\ClassLoader */
+
+$autoloader->add('Matthias\SymfonyControllerAnnotation\Tests', 'tests/');
 
 AnnotationRegistry::registerLoader(array($autoloader, 'loadClass'));
